40729e898fca93f41ea3f24416d52a703519c7df
220291p <220291p@ugs.kochi-tech.ac.jp>
2025-04-07 10:58:46 +0900

    【STEP１１】MySQL以外のDBを利用してみる

--- trainingspring-2/src/main/java/com/example/demo3/model/Person.java
+++ trainingspring-2/src/main/java/com/example/demo3/model/Person.java
@@ -1,7 +1,11 @@
 package com.example.demo3.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import lombok.Data;
